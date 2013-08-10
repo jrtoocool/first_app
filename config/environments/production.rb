0dca30666cf7a5d15e48bbdbf54a15edfeb7c545
Junior <jr_toocool@hotmail.com>
2013-08-10 01:37:39 -0500

    Regresa estado

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ FirstApp::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.initialize_on_precompile=false
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 FirstApp::Application.routes.draw do
-  
-  # root to: 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
